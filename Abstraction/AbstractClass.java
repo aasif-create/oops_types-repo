960eb0803077324d4f3dc9be6d08e27b8fbe5570
aasif-create <aasifmmd12345@gmail.com>
2025-12-23 20:28:23 +0530

    UPDATED

--- Abstraction/AbstractClass.java
+++ Abstraction/AbstractClass.java
@@ -1,22 +1,29 @@
 abstract class University
 {
     abstract void publishResult();
-    void announce() {
-        System.out.println("University has announced result publication");
-    }
-}
+    public void announce()
+    {
+        System.out.println("University announced result");
+    }}
 class College extends University {
     @Override
-    void publishResult() {
+    public void publishResult()
+    {
         System.out.println("Collecting marks from departments");
         System.out.println("Calculating grades");
         System.out.println("Publishing results to student portal");
-    }
-}
+    }}
 public class AbstractClass {
     public static void main(String[] args) {
         University obj = new College();
         obj.announce();
         obj.publishResult();
-    }
-}
+    }}
+/* 1.University is an abstract class that defines rules for
+publishing results.
+2.publishResult() is abstract, so it has no implementation details
+3.announce() is a concrete method that provides common behavior.
+4.College implements publishResult(), providing the actual logic.
+5.Main class uses the abstract class reference, hiding the child
+class implementation is called abstraction
+ */
---
+++ Abstraction/Interface.java
@@ -0,0 +1,29 @@
+interface University
+{
+    public void publishResult();
+    default void announce()
+    {
+        System.out.println("University announced results");
+    }}
+class College implements University{
+        @Override
+        public void publishResult()
+        {
+        System.out.println("Collecting marks from departments");
+        System.out.println("Calculating grades");
+        System.out.println("Publishing results to student portal");
+    }}
+public class Interface {
+        public static void main(String[] args) {
+        University obj = new College();
+        obj.announce();
+        obj.publishResult();
+    }}
+/* 1. University is an interface that declares rules for
+publishing results.
+2. publishResult() is an abstract method with no implementation in the interface.
+3. announce() is a default method that provides common behavior.
+4. College implements publishResult(), providing the actual logic.
+5. Main class uses the interface reference, hiding the
+implementation details is called abstraction.
+*/
